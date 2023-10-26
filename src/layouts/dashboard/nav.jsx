659be5de1ff320b20d375ce16008dc8f6b75a6c0
f.depalma <325587@via.dk>
2023-10-26 03:46:34 +0200

    add refresh data

--- src/layouts/dashboard/nav.jsx
+++ src/layouts/dashboard/nav.jsx
@@ -106,7 +106,32 @@ function NavItem({ item }) {
   const active = item.path === pathname;
 
   return (
-    <div></div>
+    <ListItemButton
+      component={RouterLink}
+      href={item.path}
+      sx={{
+        minHeight: 44,
+        borderRadius: 0.75,
+        typography: 'body2',
+        color: 'text.secondary',
+        textTransform: 'capitalize',
+        fontWeight: 'fontWeightMedium',
+        ...(active && {
+          color: 'primary.main',
+          fontWeight: 'fontWeightSemiBold',
+          bgcolor: (theme) => alpha(theme.palette.primary.main, 0.08),
+          '&:hover': {
+            bgcolor: (theme) => alpha(theme.palette.primary.main, 0.16),
+          },
+        }),
+      }}
+    >
+      <Box component="span" sx={{ width: 24, height: 24, mr: 2 }}>
+        {item.icon}
+      </Box>
+
+      <Box component="span">{item.title} </Box>
+    </ListItemButton>
   );
 }
 
--- src/sections/overview/view/app-view.jsx
+++ src/sections/overview/view/app-view.jsx
@@ -14,10 +14,52 @@ import AppWebsiteVisits from '../app-website-visits';
 import AppWidgetSummary from '../app-widget-summary';
 import AppTrafficBySite from '../app-traffic-by-site';
 import AppConversionRates from '../app-conversion-rates';
+import { useEffect, useState } from 'react';
 
 // ----------------------------------------------------------------------
 
 export default function AppView() {
+
+  const total_time = (Date.now()/1000).toFixed(0) - 1698281007 //1698314400
+  const prod_piece = total_time * 4
+  const defected= Number((total_time / 60).toFixed(0))
+  const complaiant=prod_piece - defected
+  const downtime= Number((total_time / 60).toFixed(0)) 
+  const availability= ((total_time - downtime)/total_time).toFixed(1)
+  const target= prod_piece + Number((total_time / 60).toFixed(0)) 
+  const performance= ((target - prod_piece)/target).toFixed(1)
+  const quality= ((prod_piece - defected)/prod_piece).toFixed(1)
+
+  const [variables, setVariables] = useState({  })
+  const [seed, setSeed] = useState(0)
+
+  useEffect(() => {
+    setInterval(() => {
+      setSeed(seed + 1)
+    }, 1000)
+  })
+
+  useEffect(() => { 
+   const intervalId = setInterval(() => {
+    const copy = {
+      total_time: (Date.now()/1000).toFixed(0) - 1698281007, // 1698314400,
+    prod_piece: variables.total_time * 4,
+    defected: Number((variables.total_time / 60).toFixed(0)),
+    complaiant: variables.prod_piece - variables.defected,
+    downtime: Number((variables.total_time / 60).toFixed(0)), 
+    availability: ((variables.total_time - variables.downtime)/variables.total_time).toFixed(1),
+    target: variables.prod_piece + Number((variables.total_time / 60).toFixed(0)), 
+    performance: ((variables.target - variables.prod_piece)/variables.target).toFixed(1),
+    quality: ((variables.prod_piece - variables.defected)/variables.prod_piece).toFixed(1)
+  }
+  setVariables(copy)
+}, 1000)
+    
+    
+    return () => clearInterval(intervalId);
+  }, [variables.defected, variables.downtime, variables.prod_piece, variables.target, variables.total_time])
+
+    
   return (
     <Container maxWidth="xl">
       <Typography variant="h4" sx={{ mb: 5 }}>
@@ -28,7 +70,7 @@ export default function AppView() {
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
             title="Piece produced"
-            total={4244}
+            total={variables.prod_piece}
             color="success"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
           />
